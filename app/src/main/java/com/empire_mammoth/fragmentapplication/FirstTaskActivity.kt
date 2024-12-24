c5a946be77bb83328131e3bb8eaf7c9bb5a07757
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2024-12-24 12:25:36 +0300

    add AFragment

--- app/src/main/java/com/empire_mammoth/fragmentapplication/FirstTaskActivity.kt
+++ app/src/main/java/com/empire_mammoth/fragmentapplication/FirstTaskActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.empire_mammoth.fragmentapplication.databinding.ActivityFirstTaskBinding
+import com.empire_mammoth.fragmentapplication.databinding.ActivityMainBinding
 
 class FirstTaskActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstTaskBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_first_task)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityFirstTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main, AFragment.newInstance())
+            .commit()
     }
 }
